7c11dbd334b4b321a1f52549b04d4cc4f0a760bf
Shajan <2053708@cognizant.com>
2021-10-16 15:29:03 +0530

    Initial checkin

---
+++ src/main/java/com/flightapp/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.flightapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/airline")
+public class TestController {
+	@GetMapping("/welcome")
+	public String allAccess() {
+		return "Welcome.";
+	}
+}
